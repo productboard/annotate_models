5e6b4e843e0f400186bb5b9fe6606364c45c0d0e
Petr Pluhar <lucker14@gmail.com>
2019-03-25 11:14:11 +0100

    save wip work on custom sort

--- lib/annotate.rb
+++ lib/annotate.rb
@@ -30,7 +30,7 @@ module Annotate
     :show_indexes, :simple_indexes, :include_version, :exclude_tests,
     :exclude_fixtures, :exclude_factories, :ignore_model_sub_dir,
     :format_bare, :format_rdoc, :format_markdown, :sort, :force, :trace,
-    :timestamp, :exclude_serializers, :classified_sort, :classified_sort_updated,
+    :timestamp, :exclude_serializers, :classified_sort, :classified_sort_updated, :custom_sort,
     :show_foreign_keys, :show_complete_foreign_keys,
     :exclude_scaffolds, :exclude_controllers, :exclude_helpers,
     :exclude_sti_subclasses, :ignore_unknown_models, :with_comment
@@ -38,7 +38,7 @@ module Annotate
   OTHER_OPTIONS = [
     :ignore_columns, :skip_on_db_migrate, :wrapper_open, :wrapper_close,
     :wrapper, :routes, :hide_limit_column_types, :hide_default_column_types,
-    :ignore_routes, :active_admin
+    :ignore_routes, :active_admin, :custom_sort_order
   ].freeze
   PATH_OPTIONS = [
     :require, :model_dir, :root_dir
